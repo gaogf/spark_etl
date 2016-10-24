724c297f6f019e9508a6d690ced07a0c5a241091
winslow <winslowyang@163.com>
2016-10-24 10:10:48 +0800

    @winslow modify ,delete not use code

--- src/main/com/unionpay/model/SampleClass.scala
+++ src/main/com/unionpay/model/SampleClass.scala
@@ -1,9 +1,11 @@
 package com.unionpay.model
 
-
-case class ProvinceCard(id: String, name: String)
-case class CityCard(id: String, name: String)
-case class CT(id: String, name: String)
+/**
+  *  Not use
+  * */
+//case class ProvinceCard(id: String, name: String)
+//case class CityCard(id: String, name: String)
+//case class CT(id: String, name: String)
 /**
   * 样例类定义
   * Created by tzq on 2016/10/13.
