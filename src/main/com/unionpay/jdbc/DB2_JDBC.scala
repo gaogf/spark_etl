0b9951324591ca5a8a144f51c4e6e0d1052e519d
winslow <winslowyang@163.com>
2016-11-15 15:26:25 +0800

    @winslowyang submit,  根据生产环境修改代码。

--- src/main/com/unionpay/jdbc/DB2_JDBC.scala
+++ src/main/com/unionpay/jdbc/DB2_JDBC.scala
@@ -11,8 +11,10 @@ import org.apache.spark.sql.{DataFrame, SQLContext}
   * Created by tzq on 2016/10/10.
   */
 object DB2_JDBC {
-  private lazy val user =ConfigurationManager.getProperty(Constants.DB2_USER)
-  private lazy val password =ConfigurationManager.getProperty(Constants.DB2_PASSWORD)
+  private lazy val user_acc =ConfigurationManager.getProperty(Constants.DB2_USER_ACC)
+  private lazy val password_acc =ConfigurationManager.getProperty(Constants.DB2_PASSWORD_ACC)
+  private lazy val user_mgm =ConfigurationManager.getProperty(Constants.DB2_USER_MGM)
+  private lazy val password_mgm =ConfigurationManager.getProperty(Constants.DB2_PASSWORD_MGM)
   private lazy val url_accdb: String =ConfigurationManager.getProperty(Constants.DB2_URL_ACCDB)
   private lazy val url_mgmdb: String =ConfigurationManager.getProperty(Constants.DB2_URL_MGMDB)
   private lazy val driver =ConfigurationManager.getProperty(Constants.DB2_DRIVER)
@@ -21,8 +23,8 @@ object DB2_JDBC {
     def jdbc_accdb_DF(table: String, numPartitions: Int = 1): DataFrame = {
       val map = Map(
         "url" -> url_accdb,
-        "user" -> user,
-        "password" -> password,
+        "user" -> user_acc,
+        "password" -> password_acc,
         "driver" -> driver,
         "dbtable" -> table,
         "numPartitions" -> s"$numPartitions")
@@ -31,8 +33,8 @@ object DB2_JDBC {
     def jdbc_mgmdb_DF(table: String, numPartitions: Int = 1): DataFrame = {
       val map = Map(
         "url" -> url_mgmdb,
-        "user" -> user,
-        "password" -> password,
+        "user" -> user_mgm,
+        "password" -> password_mgm,
         "driver" -> driver,
         "dbtable" -> table,
         "numPartitions" -> s"$numPartitions")
